d0e7d9dacd88b7882bf10a7e02e1ba602a286836
berrybab6 <bedatuassefa@gmail.com>
2022-03-23 12:14:26 +0300

    update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,12 +17,12 @@ export default function Sidebar() {
             >
                 <div className="flex-col items-stretch min-h-full flex-nowrap px-0 relative">
                     <a
-                        href="https://material-tailwind.com?ref=mtd"
+                        // href="https://material-tailwind.com?ref=mtd"
                         target="_blank"
                         rel="noreferrer"
                         className="mt-2 text-center w-full inline-block"
                     >
-                        <H6 color="gray">Material Tailwind</H6>
+                        <H6 color="gray">Telecommunication</H6>
                     </a>
                     <div className="flex flex-col">
                         <hr className="my-4 min-w-full" />
@@ -71,7 +71,7 @@ export default function Sidebar() {
                             </li>
                             <li className="px-4 rounded-lg mb-2 text-gray-700">
                                 <a
-                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/login"
+                                    // href="https://demos.creative-tim.com/material-tailwind-kit-react/#/login"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="flex items-center gap-4 text-sm font-light py-3"
@@ -82,7 +82,7 @@ export default function Sidebar() {
                             </li>
                             <li className="px-4 rounded-lg mb-2 text-gray-700">
                                 <a
-                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/register"
+                                    // href="https://demos.creative-tim.com/material-tailwind-kit-react/#/register"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="flex items-center gap-4 text-sm font-light py-3"
@@ -93,7 +93,7 @@ export default function Sidebar() {
                             </li>
                             <li className="px-4 rounded-lg mb-2 text-gray-700">
                                 <a
-                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    // href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="flex items-center gap-4 text-sm font-light py-3"
@@ -104,7 +104,7 @@ export default function Sidebar() {
                             </li>
                             <li className="px-4 rounded-lg mb-2 text-gray-700">
                                 <a
-                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                    // href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="flex items-center gap-4 text-sm font-light py-3"
@@ -115,29 +115,9 @@ export default function Sidebar() {
                             </li>
                         </ul>
 
-                        <ul className="flex-col min-w-full flex list-none absolute bottom-0">
-                            <li className="bg-gradient-to-tr from-light-blue-500 to-light-blue-700 px-4 rounded-lg text-white mb-2">
-                                <a
-                                    href="https://material-tailwind.com/documentation/quick-start"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="flex items-center gap-4 text-sm font-light py-3"
-                                >
-                                    <Icon name="description" size="2xl" />
-                                    Documentation
-                                </a>
-                            </li>
-                            <li className="bg-gradient-to-tr from-purple-500 to-purple-700 px-4 rounded-lg text-white">
-                                <a
-                                    href="https://www.creative-tim.com/product/material-tailwind-dashboard-react"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="flex items-center justify-center gap-4 text-sm font-light py-3"
-                                >
-                                    Free Download
-                                </a>
-                            </li>
-                        </ul>
+             
+                           
+                                 
                     </div>
                 </div>
             </div>
