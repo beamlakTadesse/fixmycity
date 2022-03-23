dcb288ec918aecb78f8ad4665e8ac231908401da
berrybab6 <bedatuassefa@gmail.com>
2022-03-23 12:16:16 +0300

    create sectors dashboard

---
+++ src/pages/SectorAdmin/Dashboard.js
@@ -0,0 +1,94 @@
+import StatusCard from 'components/StatusCard';
+import ChartLine from 'components/sector/ChartLine';
+import ChartBar from 'components/ChartBar';
+import PageVisitsCard from 'components/sector/PageVisitsCard';
+import TrafficCard from 'components/TrafficCard';
+
+export default function Dashboard() {
+    return (
+        <>
+            <div className="bg-light-blue-500 px-3 md:px-8 h-40" />
+
+            <div className="px-3 md:px-8 -mt-24">
+
+
+
+            <div className="px-3 md:px-8">
+                <div className="container mx-auto max-w-full">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 mb-4">
+                        <StatusCard
+                            color="pink"
+                            icon="trending_up"
+                            title="Active Reports"
+                            amount="350,897"
+                            percentage="3.48"
+                            percentageIcon="arrow_upward"
+                            percentageColor="green"
+                            date="Since last month"
+                        />
+                        <StatusCard
+                            color="orange"
+                            icon="groups"
+                            title="Resolved Reports"
+                            amount="2,356"
+                            percentage="3.48"
+                            percentageIcon="arrow_downward"
+                            percentageColor="red"
+                            date="Since last week"
+                        />
+                       
+                          
+    
+                          <StatusCard
+                            color="blue"
+                            icon="poll"
+                            title="Spam Reports"
+                            amount="49,65%"
+                            percentage="12"
+                            percentageIcon="arrow_upward"
+                            percentageColor="green"
+                            date="Since last month"
+                        />
+                        <StatusCard
+                            color="blue"
+                            icon="poll"
+                            title="Number of Branchs"
+                            amount="49,65%"
+                            percentage="12"
+                            percentageIcon="arrow_upward"
+                            percentageColor="green"
+                            date="Since last month"
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+                <div className="container mx-auto max-w-full">
+
+
+                    <div className="grid grid-cols-1 xl:grid-cols-5">
+                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
+                            <ChartLine />
+                        </div>
+                        <div className="xl:col-start-4 xl:col-end-6 px-4 mb-14">
+                            <PageVisitsCard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+      <div className="px-3 md:px-8 h-auto">
+                <div className="container mx-auto max-w-full">
+                    <div className="grid grid-cols-1 xl:grid-cols-5">
+                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
+                            <PageVisitsCard />
+                        </div>
+                        <div className="xl:col-start-4 xl:col-end-6 px-4 mb-14">
+                            <TrafficCard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
