863c81e28cfa39a7951abaaa317d29b03f874c7e
berrybab6 <bedatuassefa@gmail.com>
2022-03-25 02:24:10 +0300

    update announcement page

--- src/pages/SectorAdmin/Announcement.js
+++ src/pages/SectorAdmin/Announcement.js
@@ -9,36 +9,30 @@ import LargeButton from 'components/LargeButton';
 import Announcement_Card from 'components/sector/Announcement/mycard';
 
 export default function Announcement() {
+
+
+    const products = ['Custom users', 'Banned Users', 'Sectors'];
+
+    const list = products.map(product => <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14 mr-8">
+
+    {/* <PageVisitsCard /> */}
+    <Announcement_Card />
+    
+</div>)
+  
+
+
     return (
         <>
             <div className="bg-light-blue-500 px-3 md:px-8 h-10" />
 
-            {/* <div className="px-3 md:px-8 -mt-24"> */}
-            {/* <div className="px-3 md:px-8 -mt-24"> */}
-
-{/* 
-
-            <div className="px-3 md:px-8">
-                <div className="container mx-auto max-w-full">
-                    
-                </div>
-            </div> */}
-
-
                 <div className="container mt-10 max-w-full ">
 
-
-                    <div className="grid grid-cols-1 xl:grid-cols-1">
+                <div className="grid grid-cols-1 xl:grid-cols-1">
                     <div className="xl:col-start-2 xl:col-end-2 px-3 mb-14 mr-8">
-
-                             <LargeButton />
-                        </div> 
-                        {/* <div className="xl:col-start-2 xl:col-end-2 px-3 mb-14 mr-8"> */}
-                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14 mr-8">
-
-                            {/* <PageVisitsCard /> */}
-                            <Announcement_Card />
-                        </div>
+                         <LargeButton />
+                    </div> 
+                        {list}
                     </div>
                 </div>
             {/* </div> */}
