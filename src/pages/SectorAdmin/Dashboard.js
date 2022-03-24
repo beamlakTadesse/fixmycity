96da9982faaf80f688bf2c6bd2837de1c9feb567
berrybab6 <bedatuassefa@gmail.com>
2022-03-24 10:27:33 +0300

    update sector dashboard

--- src/pages/SectorAdmin/Dashboard.js
+++ src/pages/SectorAdmin/Dashboard.js
@@ -3,6 +3,7 @@ import ChartLine from 'components/sector/ChartLine';
 import ChartBar from 'components/ChartBar';
 import PageVisitsCard from 'components/sector/PageVisitsCard';
 import TrafficCard from 'components/TrafficCard';
+import CardTable from 'components/sector/TableCard';
 
 export default function Dashboard() {
     return (
@@ -79,13 +80,10 @@ export default function Dashboard() {
             </div>
       <div className="px-3 md:px-8 h-auto">
                 <div className="container mx-auto max-w-full">
-                    <div className="grid grid-cols-1 xl:grid-cols-5">
-                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
-                            {/* <PageVisitsCard /> */}
-                        </div>
-                        <div className="xl:col-start-4 xl:col-end-6 px-4 mb-14">
-                            {/* <TrafficCard /> */}
-                        </div>
+                    <div className="grid grid-cols-1 xl:grid-cols-1">
+                        
+                            <CardTable/>
+                        
                     </div>
                 </div>
             </div>
