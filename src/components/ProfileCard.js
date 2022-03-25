6b5d02e2b1f6d7aa03c7274f3a80ae4d1e385f1a
@nahilishasho <shashonahili702@gmail.com>
2022-03-25 07:37:42 +0300

    profile

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,11 +1,10 @@
 import Card from '@material-tailwind/react/Card';
-import CardBody from '@material-tailwind/react/CardBody';
-import CardFooter from '@material-tailwind/react/CardFooter';
 import Image from '@material-tailwind/react/Image';
-import H5 from '@material-tailwind/react/Heading5';
-import Icon from '@material-tailwind/react/Icon';
-import LeadText from '@material-tailwind/react/LeadText';
+import CardHeader from '@material-tailwind/react/CardHeader';
+import CardBody from '@material-tailwind/react/CardBody';
 import Button from '@material-tailwind/react/Button';
+import Input from '@material-tailwind/react/Input';
+import Textarea from '@material-tailwind/react/Textarea';
 import ProfilePicture from 'assets/img/team-1-800x800.jpg';
 
 export default function ProfileCard() {
@@ -15,49 +14,56 @@ export default function ProfileCard() {
                 <div className="w-48 px-4 -mt-24">
                     <Image src={ProfilePicture} rounded raised />
                 </div>
-                <div className="w-full flex justify-center py-4 lg:pt-4 pt-8">
-                    
                 </div>
-            </div>
-            <div className="text-center">
-                <H5 color="gray">John Smith</H5>
-                <div className="mt-0 mb-2 text-gray-700 flex items-center justify-center gap-2">
-                    <Icon name="place" size="xl" />
-                    Los Angeles, California
-                </div>
-                <div className="mb-2 text-gray-700 mt-10 flex items-center justify-center gap-2">
-                    <Icon name="work" size="xl" />
-                    Solution Manager - Creative Tim Officer
-                </div>
-                <div className="mb-2 text-gray-700 flex items-center justify-center gap-2">
-                    <Icon name="account_balance" size="xl" />
-                    University of Computer Science
-                </div>
-            </div>
+
             <CardBody>
-                <div className="border-t border-lightBlue-200 text-center px-2 ">
-                    <LeadText color="blueGray">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
-                    </LeadText>
-                </div>
+            
+                <form>
+                    <h6 className="text-purple-500 text-sm mt-9 mb-6 font-bold uppercase">
+                        personal Information
+                    </h6>
+                    <div className="flex flex-wrap mt-10">
+                        <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
+                            <h1>Full name : John Doe</h1>
+                            
+                        </div>
+                        <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
+                            <h1>Email : Johndoe@gmail.com</h1>
+                        </div>
+                        <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
+                            <h1>Location : Addis Ababa / Ethiopia</h1>
+                        </div>
+                        <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
+                            <h1>Phone No. : +251 9867888899</h1>
+                        </div>
+                    </div>
+
+                    <h6 className="text-purple-500 text-sm my-6 font-bold uppercase">
+                        Sector Information
+                    </h6>
+                    <div className="flex flex-wrap mt-10">
+                        <div className="w-full lg:w-12/12 mb-10 font-light">
+                            <h1>Sector : ELPA</h1>
+                        </div>
+                        <div className="w-full lg:w-4/12 pr-4 mb-10 font-light">
+                            <h1>Location : Addis Ababa / Ethiopia</h1>
+                        </div>
+                        <div className="w-full lg:w-4/12 px-4 mb-10 font-light">
+                            <h1>Email : Elpa@gmail.com</h1>
+                        </div>
+                        <div className="w-full lg:w-4/12 pl-4 mb-10 font-light">
+                            <h1>Phone No. : +251 98783733</h1>
+                        </div>
+                    </div>
+
+                    {/* <h6 className="text-purple-500 text-sm my-6 font-light uppercase">
+                        About Me
+                    </h6> */}
+                    {/* <div className="flex flex-wrap mt-10 font-light">
+                        <Textarea color="purple" placeholder="About Me" />
+                    </div> */}
+                </form>
             </CardBody>
-            <CardFooter>
-                <div className="w-full flex justify-center -mt-8">
-                    <a
-                        href="#pablo"
-                        className="mt-5"
-                        onClick={(e) => e.preventDefault()}
-                    >
-                        <Button color="purple" buttonType="link" ripple="dark">
-                            Show more
-                        </Button>
-                    </a>
-                </div>
-            </CardFooter>
         </Card>
     );
 }
