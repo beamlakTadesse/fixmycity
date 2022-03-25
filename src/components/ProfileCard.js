c7c9e340b64aa86fa51c1e0b51eca84c564a85f3
@nahilishasho <shashonahili702@gmail.com>
2022-03-25 08:01:38 +0300

    profile

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -22,38 +22,49 @@ export default function ProfileCard() {
                     <h6 className="text-purple-500 text-sm mt-9 mb-6 font-bold uppercase">
                         personal Information
                     </h6>
-                    <div className="flex flex-wrap mt-10">
+                    <div className="flex flex-wrap text-xs mt-10">
                         <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
-                            <h1>Full name : John Doe</h1>
+                            <h1>Full name : </h1>
+                            <h1>John Doe</h1>
                             
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
-                            <h1>Email : Johndoe@gmail.com</h1>
+                            <h1>Email : </h1>
+                            <h1>Johndoe@gmail.com</h1>
                         </div>
                         <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
-                            <h1>Location : Addis Ababa / Ethiopia</h1>
+                            <h1>Location :</h1>
+                            <h1> Addis Ababa / Ethiopia</h1>
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
-                            <h1>Phone No. : +251 9867888899</h1>
+                            <h1>Phone No. : </h1>
+                            <h1>+251 9867888899</h1>
                         </div>
+                        
                     </div>
 
                     <h6 className="text-purple-500 text-sm my-6 font-bold uppercase">
                         Sector Information
                     </h6>
-                    <div className="flex flex-wrap mt-10">
-                        <div className="w-full lg:w-12/12 mb-10 font-light">
+                    <div className="flex flex-wrap text-xs mt-10">
+                        <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
                             <h1>Sector : ELPA</h1>
+                            <h1>ELPA</h1>
+                            
                         </div>
-                        <div className="w-full lg:w-4/12 pr-4 mb-10 font-light">
-                            <h1>Location : Addis Ababa / Ethiopia</h1>
+                        <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
+                            <h1>Location : </h1>
+                            <h1>Addis Ababa / Ethiopia</h1>
                         </div>
-                        <div className="w-full lg:w-4/12 px-4 mb-10 font-light">
-                            <h1>Email : Elpa@gmail.com</h1>
+                        <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
+                            <h1>Email : </h1>
+                            <h1> Elpa@gmail.com</h1>
                         </div>
-                        <div className="w-full lg:w-4/12 pl-4 mb-10 font-light">
-                            <h1>Phone No. : +251 98783733</h1>
+                        <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
+                            <h1>Phone No. : </h1>
+                            <h1>+251 9867888899</h1>
                         </div>
+                        
                     </div>
 
                     {/* <h6 className="text-purple-500 text-sm my-6 font-light uppercase">
