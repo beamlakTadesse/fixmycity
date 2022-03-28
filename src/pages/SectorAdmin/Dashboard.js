00bc686eac9b58aa18e0f911def69e9d13787fdf
berrybab6 <bedatuassefa@gmail.com>
2022-03-29 01:07:57 +0300

    update Sector Dashboard

--- src/pages/SectorAdmin/Dashboard.js
+++ src/pages/SectorAdmin/Dashboard.js
@@ -12,8 +12,6 @@ export default function Dashboard() {
 
             <div className="px-3 md:px-8 -mt-24">
 
-
-
             <div className="px-3 md:px-8">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 mb-4">
@@ -38,8 +36,6 @@ export default function Dashboard() {
                             date="Since last week"
                         />
                        
-                          
-    
                           <StatusCard
                             color="blue"
                             icon="poll"
