f0d3e774b9cdd36aec9408a539fdeb87b859689e
berrybab6 <bedatuassefa@gmail.com>
2022-04-01 22:49:20 +0300

    update reducer/announcement file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { users } from './user.reducer';
 import { alert } from './alert.reducer';
 import { sectors } from './sector.reducer';
+import { announcement } from './announcement.reducer';
 
 const rootReducer = combineReducers({
 
     users,
     alert,
-    sectors
+    sectors,
+    announcement
 });
 
 export default rootReducer;
