d844cd795197f75836ecb7e055d4cc0c391667aa
berrybab6 <bedatuassefa@gmail.com>
2022-04-15 00:44:11 +0300

    edit profileCard

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -6,13 +6,41 @@ import Button from '@material-tailwind/react/Button';
 import Input from '@material-tailwind/react/Input';
 import Textarea from '@material-tailwind/react/Textarea';
 import ProfilePicture from 'assets/img/team-1-800x800.jpg';
-
+import React, {useState, useEffect} from 'react';
 export default function ProfileCard() {
+
+
+    const [userId, setUserId] = useState(4);
+    const [users , setUsers] = useState({});
+    useEffect(() => {
+        // mounted.current = true;
+        const url = `http://localhost:8000/v1/admins/users/${userId}`;
+
+
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url);
+           
+            const json = await response.json();
+            
+            setUsers(json.user);
+           
+            // console.log("Sectors: ", json.sectors[0].district_name);
+          } catch (error) {
+            console.log("error", error);
+          }
+        };
+
+          fetchData();
+      
+    }, []);
     return (
+        <>
+        {users &&
         <Card>
             <div className="flex flex-wrap justify-center">
                 <div className="w-48 px-4 -mt-24">
-                    <Image src={ProfilePicture} rounded raised />
+                    <Image src={users.image} rounded raised />
                 </div>
                 </div>
 
@@ -25,12 +53,12 @@ export default function ProfileCard() {
                     <div className="flex flex-wrap text-xs mt-10">
                         <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
                             <h1>Full name : </h1>
-                            <h1>John Doe</h1>
+                            <h1>{users.full_name}</h1>
                             
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
-                            <h1>Email : </h1>
-                            <h1>Johndoe@gmail.com</h1>
+                            <h1>Username : </h1>
+                            <h1>{users.username}</h1>
                         </div>
                         <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
                             <h1>Location :</h1>
@@ -38,7 +66,7 @@ export default function ProfileCard() {
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
                             <h1>Phone No. : </h1>
-                            <h1>+251 9867888899</h1>
+                            <h1>{users.phone_number}</h1>
                         </div>
                         
                     </div>
@@ -76,5 +104,7 @@ export default function ProfileCard() {
                 </form>
             </CardBody>
         </Card>
+                }
+                </>
     );
 }
