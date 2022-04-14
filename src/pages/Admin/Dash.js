ad3792eae000b22fcb4a10774dbd6292f07ea34b
berrybab6 <bedatuassefa@gmail.com>
2022-04-15 00:49:22 +0300

    update Admin dashboard page

--- src/pages/Admin/Dash.js
+++ src/pages/Admin/Dash.js
@@ -9,33 +9,53 @@ import StatusCard from 'components/StatusCard';
 import ChartBar from 'components/admin/ChartBar';
 // import PageVisitsCard from 'components/PageVisitsCard';
 // import TrafficCard from 'components/TrafficCard';
-import SectorManage from 'components/SectorManage';
-import React, { useEffect, useState} from 'react';
+import SectorManage from 'components/admin/SectorManage';
+import React, { useEffect, useState, useRef} from 'react';
 
 export default function Dashboard() {
 
 
-  
+    const mounted = useRef(false);
+
     const [data ,setData] = useState({});
+    const [data2, setData2] = useState({});
+    const [sectors, setSectors] = useState({});
 
     useEffect(() => {
+        mounted.current = true;
+
         const url = `http://localhost:8000/v1/admins/user_count/`;
-    
+        const url2 = `http://localhost:8000/v1/admins/sector_count/`;
+        const url3 = `http://localhost:8000/v1/admins/active_sectors/`;
+
+
         const fetchData = async () => {
           try {
             const response = await fetch(url);
+            const response2 = await fetch(url2);
+            const response3 = await fetch(url3);
+
             const json = await response.json();
+            const json2 = await response2.json();
+            const json3 = await response3.json();
+
             setData(json);
+            setData2(json2);
+            setSectors(json3);
             console.log(json.banned);
+            console.log(json2.water);
+            console.log(json3[1])
+
           } catch (error) {
             console.log("error", error);
           }
         };
-    
-        fetchData();
+
+          fetchData();
+      
     }, []);
 
-   
+    var list2 = <ChartBar medata = {data2}/> 
   
     return (
         <>
@@ -61,14 +81,12 @@ export default function Dashboard() {
                                 icon="group"
                                 title = {data[oneKey][1]}
                                 amount={data[oneKey][0]}
-                                percentage="1.10"
-                                percentageIcon="arrow_downward"
-                                percentageColor="orange"
                                 date="Since yesterday"
                             />
 
                     )
-                })
+                }) 
+                
 
             }
 
@@ -76,15 +94,18 @@ export default function Dashboard() {
                   
                     </div>
                 </div>
+            
+            
             </div>
 
                     <div className="grid grid-cols-1 xl:grid-cols-2 px-4 mb-14">
                             {/* <ChartLine /> */}
                             <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
-                            <ChartBar />
+                              <ChartBar medata = {data2}/>
+                           
                         </div>
                         <div className="xl:col-start-4 xl:col-end-6 px-4 mb-14">
-                            <SectorManage />
+                            <SectorManage sectorsCount = {sectors}/>
                         </div>
                             {/* <ChartBar /> */}
                       
