0c92802039680e8e01aceef9174858a2f88186f8
@nahilishasho <shashonahili702@gmail.com>
2022-04-15 07:57:44 +0300

    edditing report detail

--- src/components/sector/ReportInfo.js
+++ src/components/sector/ReportInfo.js
@@ -1,7 +1,28 @@
+import Image1 from '../../assets/img/tick.jpg';
+import Image2 from '../../assets/img/profile.jpg';
 export default function ReportInfo() {
     return(
-        <div>
-            report page
-        </div>
+        <div className="w-[500px] border-2 border-solid ml-6 mt-7">
+         <div className = "border-2 border-solid  w-[300px] h-[150px] mt-[40px] ml-[20px] rounded-xl drop-shadow-2xl">
+         <img 
+             src= {Image1}
+             className = " w-[90px] ml-[10px] -mt-[30px] drop-shadow-2xl rounded-xl"/>
+            <p className="text-inherit  ml-[180px] text-2xl">Resolved</p>
+            <hr className=""></hr>
+            <p className=" mt-[13px] ml-[100px]">1 month ago</p>
+
+         </div>
+         <div className = "border-2 border-solid  w-[300px] h-[150px] mt-[40px] ml-[20px] rounded-xl drop-shadow-2xl">
+         <img 
+             src= {Image2 }
+             className = " w-[90px] ml-[10px] -mt-[30px] drop-shadow-2xl rounded-xl"/>
+            
+            <p className="text-inherit  ml-[180px] text-2xl">John Doe</p>
+            <hr className=""></hr>
+            <p className=" mt-[13px] ml-[100px]">Total : 11 reports</p>
+
+         </div>
+
+      </div>
     );
 }
--- src/pages/Report.js
+++ src/pages/Report.js
@@ -3,7 +3,7 @@ import ReportInfo from "components/sector/ReportInfo";
 
 export default function Report() {
     return(
-        <div className = "flex m-5 h-screen shrink">
+        <div className = "flex m-5 h-screen shrink " style={{backgroundColor: 'rgb(194 65 12)'}}>
             <ReportDetail/>
             <ReportInfo/>
         </div>
