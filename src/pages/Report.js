a8181e5c2ec2574aa9c4ce219780b612fa3b066f
@nahilishasho <shashonahili702@gmail.com>
2022-04-15 06:22:53 +0300

    Editing reportDetail

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,9 +1,11 @@
 import ReportDetail from "components/sector/ReportDetail";
+import ReportInfo from "components/sector/ReportInfo";
 
 export default function Report() {
     return(
         <div flex m-5 h-screen shrink>
             <ReportDetail/>
+            <ReportInfo/>
         </div>
     );
 }
