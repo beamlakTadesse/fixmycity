f71cfecd71fafc696a9a447cfe74cf8165d4eb46
berrybab6 <bedatuassefa@gmail.com>
2022-04-15 15:51:44 +0300

    update report Table

--- src/components/report/reportTable.js
+++ src/components/report/reportTable.js
@@ -17,49 +17,7 @@ export default function ReportTable() {
 
     const report = useSelector(state => state.report);
 
-    // const user = useSelector(state => state.authentication.user);
-    const dispatch = useDispatch();
-
-
-const [mydata ,setData]= useState([]);
-
-// useEffect(() => {
-//   // mounted.current = true;
-//   const url = `http://localhost:8000/v1/report/`;
-
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch(url);
-     
-//       const json = await response.json();
-//       const lela = []
-//         setData(json);
-       
-//       console.log("Sectors: ", json[0].id);
-//     } catch (error) {
-//       console.log("error", error);
-//     }
-//   };
-
-//     fetchData();
-
-// }, [mydata]);
-
-    // useEffect(() => {
-
-    //     dispatch(reportActions.getAll());
-
-    //     function toJSONLocal(date) {
-    //         var local = new Date(date);
-    //         local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-    //         return local.toJSON().slice(0, 10);
-    //     }
-    // }, []);
-
-    function toDate(date) {
-        return new Date(date).toDateString();
-    }
+    
     return (
         <Card>
             <CardHeader color="purple" contentPosition="left">
