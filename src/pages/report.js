871611f50a41b874f6227d72bf62d90b19f79368
berrybab6 <bedatuassefa@gmail.com>
2022-05-13 13:29:57 +0300

    update src/pages/report.js

--- src/pages/report.js
+++ src/pages/report.js
@@ -2,12 +2,12 @@ import StatusCard from 'components/StatusCard';
 import MapExample from 'components/MapExample';
 import UserTable from 'components/user/userTable';
 import TabsRender from 'components/tabs'
-import ReportTable from 'components/report/reportTable';
+import ReportShow from 'components/report/ReportShow';
 export default function Report() {
     return (
         <>
 
-            <ReportTable />
+            <ReportShow />
 
             {/* <UserTable /> */}
         </>
