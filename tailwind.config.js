8753598468dfb89122f14c6af3cfb9dac216e2ff
berrybab6 <bedatuassefa@gmail.com>
2022-05-13 13:32:55 +0300

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,11 @@ module.exports = {
                     '0 35px 35px rgba(0, 0, 0, 0.25)',
                     '0 45px 65px rgba(0, 0, 0, 0.15)'
                 ]
-              }
+              },
+            animation: {
+                bounce200: 'bounce 1s infinite 200ms',
+                bounce400: 'bounce 1s infinite 400ms',
+            },
         },
     },
     variants: {
