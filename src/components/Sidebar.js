4d97c279a9884b4874a565bfaf8144529c85d81a
@nahilishasho <shashonahili702@gmail.com>
2022-05-17 10:36:49 +0300

    .

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -44,7 +44,7 @@ export default function Sidebar() {
                                 <NavLink
                                     to="/admin/sectors"
                                     className="flex items-center gap-4 text-sm text-gray-700 font-light px-4 py-3 rounded-lg"
-                                    activeClassName="bg-gradient-to-tr from-light-blue-500 to-light-blue-700 text-white shadow-md"
+                                    activeClassName="bg-gradient-to-tr from-[#481F01] to-[#4A2511] text-white shadow-md"
                                 >
                                     <Icon name="settings" size="2xl" />
                                     Sector
