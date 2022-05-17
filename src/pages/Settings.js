6902e31d46a2dcc176fa28db97c007f01e37594a
berrybab6 <bedatuassefa@gmail.com>
2022-05-17 05:20:18 +0300

    update user profilepage

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,11 @@
 import StatusCard from 'components/StatusCard';
 import SettingsForm from 'components/SettingsForm';
 import ProfileCard from 'components/ProfileCard';
+import { useState } from 'react';
 
 export default function Dashboard() {
+    const [editProfile, setEditProfile]= useState(false);
+
     return (
         <>
             <div className="bg-light-blue-500 pt-14 pb-28 px-3 md:px-8 h-auto">
@@ -12,13 +15,15 @@ export default function Dashboard() {
             <div className="px-3 md:px-8 h-auto -mt-24">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 xl:grid-cols-6">
-                        <div className="xl:col-start-1 xl:col-end-5 px-4 mb-16">
+                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-16">
 
-                            <ProfileCard />
+                            <ProfileCard editProfile={editProfile} setEditProfile={setEditProfile}/>
                         </div>
-                        <div className="xl:col-start-5 xl:col-end-7 px-4 mb-16 mt-14">
-                            <SettingsForm />
+                        {editProfile &&
+                        <div className="xl:col-start-4 xl:col-end-7 px-4 mb-16 ">
+                            <SettingsForm editProfile={editProfile} setEditProfile={setEditProfile}/>
                         </div>
+                }
                     </div>
                 </div>
             </div>
