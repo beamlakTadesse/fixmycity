aa63c95470601655b1c98f46eac46c9da7654c39
@nahilishasho <shashonahili702@gmail.com>
2022-05-18 08:11:00 +0300

    editing addSectorForms page

--- src/components/sector/addSectorForm.js
+++ src/components/sector/addSectorForm.js
@@ -105,7 +105,7 @@ export default function AddSectorForm() {
                         </div>
                         <div className="row-span-3">
 
-                            <Button >
+                            <Button color = 'brown' >
                                 Cancel
                             </Button>
                         </div>
